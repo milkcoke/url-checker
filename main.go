be6c5282bd5dfef0d9baecbcde05ae327838c65c
문승훈 <mbh023@gmail.com>
2021-10-09 14:47:38 +0900

    feat: Test channel on main with goroutine

--- main.go
+++ main.go
@@ -4,13 +4,14 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"strconv"
 	"time"
 )
 
 // Goroutines 다른함수와 동시에 실행시키는 것.
 // goroutine is alive as long a program 'main function' is running
 // if main function finished, goroutine should be finished
-// main function doesn't wait goroutine
+// main function doesn't wait goroutine itself
 
 // How to communicate between main and goroutine ?
 // We can send result from goroutine!
@@ -23,6 +24,20 @@ func printCount(name string) {
 	for i := 0; i < 10; i++ {
 		println(name, i)
 		time.Sleep(time.Second)
+	}
+}
+
+func waitAndCheckOdd(num int, channel chan string) {
+
+	for i := 0; i < 10; i++ {
+		println("wait for result about : ", num, i, "seconds...")
+		time.Sleep(time.Second)
+	}
+
+	if num%2 == 0 {
+		channel <- "false: " + strconv.Itoa(num)
+	} else {
+		channel <- "true " + strconv.Itoa(num)
 	}
 }
 
@@ -49,35 +64,48 @@ func hitUrl(url string) error {
 }
 
 func main() {
+	/*
+		// make empty map
+		results := make(map[string]string)
 
-	results := make(map[string]string)
-
-	urls := []string{
-		"https://www.airbnb.com",
-		"https://www.google.com",
-		"https://www.naver.com/",
-		"https://www.facebook.com/",
-	}
-
-	for _, url := range urls {
-		error := hitUrl(url)
-		if error != nil {
-			// error type is an interface type that has an interface
-			//type error interface {
-			//	Error() string
-			//}
-			results[url] = error.Error()
-		} else {
-			results[url] = "Success"
+		urls := []string{
+			"https://www.airbnb.com",
+			"https://www.google.com",
+			"https://www.naver.com/",
+			"https://www.facebook.com/",
 		}
-	}
 
-	for url, resultString := range results {
-		fmt.Println(url, " : ", resultString)
-	}
+		for _, url := range urls {
+			error := hitUrl(url)
+			if error != nil {
+				// error type is an interface type that has an interface
+				//type error interface {
+				//	Error() string
+				//}
+				results[url] = error.Error()
+			} else {
+				results[url] = "Success"
+			}
+		}
+
+		for url, resultString := range results {
+			fmt.Println(url, " : ", resultString)
+		}
+	*/
 
 	// goroutine test
-	go printCount("kkk")
-	printCount("ccc")
+
+	// make channel
+	channel := make(chan string)
+	go waitAndCheckOdd(5, channel)
+	go waitAndCheckOdd(2, channel)
+	// When main function watch '<-' block the code (wait the message)
+	// and getting a message from the channel
+	// <- is receiver!
+	fmt.Println("Received message from the channel : ", <-channel)
+	fmt.Println("Received message from the channel : ", <-channel)
+
+	// Go knows how many goroutines are running
+	// and administrate goroutine state (message queue)
 
 }

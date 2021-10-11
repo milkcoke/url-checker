54ed2cb7041d3c435fb6ed6ac341311cd3be3173
SungHun Moon <seunghunmoon023@gene1on1.com>
2021-10-11 15:20:01 +0900

    feat: Use goroutine in for loop with IIFE
    There is an issue of range-based for loop
    
    See also: https://www.pixelstech.net/article/1583648868-Some-tricks-and-tips-for-using-for-range-in-GoLang

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"log"
 	"net/http"
 	"strconv"
 	"time"
@@ -19,6 +20,11 @@ import (
 var successCode = [4]int{200, 201, 202, 204}
 var errRequestFailed = errors.New("Request failed!")
 var errInvalidRequest = errors.New("Invalid Request!")
+
+type responseResult struct {
+	url        string
+	statusCode int
+}
 
 func printCount(name string) {
 	for i := 0; i < 10; i++ {
@@ -51,11 +57,18 @@ func isSuccess(statusCode int) bool {
 	return false
 }
 
-func hitUrl(url string) error {
+// send only channel parameter
+func hitUrl(url string, channel chan<- responseResult) error {
+	//fmt.Println("전달받은 주소 : ", url)
 	response, error := http.Get(url)
+	// send 전용인지 receive 전용인지 명확하게 명시.
+
 	if error != nil {
 		return errRequestFailed
 	}
+	// send data to channel
+	channel <- responseResult{url: url, statusCode: response.StatusCode}
+
 	if isSuccess(response.StatusCode) {
 		return nil
 	} else {
@@ -64,48 +77,67 @@ func hitUrl(url string) error {
 }
 
 func main() {
-	/*
-		// make empty map
-		results := make(map[string]string)
 
-		urls := []string{
-			"https://www.airbnb.com",
-			"https://www.google.com",
-			"https://www.naver.com/",
-			"https://www.facebook.com/",
-		}
+	channel := make(chan responseResult)
 
-		for _, url := range urls {
-			error := hitUrl(url)
-			if error != nil {
-				// error type is an interface type that has an interface
-				//type error interface {
-				//	Error() string
-				//}
-				results[url] = error.Error()
-			} else {
-				results[url] = "Success"
+	urls := []string{
+		"https://www.airbnb.com",
+		"https://www.google.com",
+		"https://www.naver.com/",
+		"https://www.facebook.com/",
+	}
+
+	//waitGroup := sync.WaitGroup{}
+
+	// goroutine!
+	for index, url := range urls {
+		// value_temp = range_temp[index_temp]
+		// index = index_temp
+		// value = value_temp
+
+		fmt.Printf("index : %d, url: %s\n", index, url)
+		go func() {
+			//defer waitGroup.Done()
+			// 여기로 url 이 왜 다 안넘어가지?
+			fmt.Println("index in goroutine : ", index)
+			err := hitUrl(url, channel)
+			if err != nil {
+				log.Fatalln(err)
 			}
-		}
+		}()
+		time.Sleep(time.Millisecond * 300)
+		fmt.Printf("22222 index : %d, url: %s\n", index, url)
+		//go func(url string) {
+		//	//defer waitGroup.Done()
+		//	// 여기로 url 이 왜 다 안넘어가지?
+		//	err := hitUrl(url, channel)
+		//	if err != nil {
+		//		log.Fatalln(err)
+		//	}
+		//}(url)
+	}
 
-		for url, resultString := range results {
-			fmt.Println(url, " : ", resultString)
-		}
-	*/
+	// 여기서는 range-based for loop 보다
+	// 호출 횟수를 여러번하는게 좋음.
+	// 호출 순서 상관없이 channel Message Queue (FIFO Queue) 에 먼저 추가된 메시지부터 아무거나 받아오기 때문
+	for i := 0; i < len(urls); i++ {
+		fmt.Printf("result: %v\n", <-channel)
+	}
 
 	// goroutine test
 
-	// make channel
-	channel := make(chan string)
-	go waitAndCheckOdd(5, channel)
-	go waitAndCheckOdd(2, channel)
-	// When main function watch '<-' block the code (wait the message)
-	// and getting a message from the channel
-	// <- is receiver!
-	fmt.Println("Received message from the channel : ", <-channel)
-	fmt.Println("Received message from the channel : ", <-channel)
+	/*
+		// make channel
+		channel := make(chan string)
+		go waitAndCheckOdd(5, channel)
+		go waitAndCheckOdd(2, channel)
+		// When main function watch '<-' block the code (wait the message)
+		// and getting a message from the channel
+		// <- is receiver!
+		fmt.Println("Received message from the channel : ", <-channel)
+		fmt.Println("Received message from the channel : ", <-channel)
 
-	// Go knows how many goroutines are running
-	// and administrate goroutine state (message queue)
-
+		// Go knows how many goroutines are running
+		// and administrate goroutine state (message queue)
+	*/
 }
